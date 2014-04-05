d239a555fafad971bbc1531445b249b70687e2b5
Sinoxan <sinoxan@googlemail.com>
2014-04-06 00:40:57 +0100

    Correcting fields for next quest in chain

--- updates/quest_template.sql
+++ updates/quest_template.sql
@@ -5,14 +5,18 @@
 -- Special Forces       (574)
 -- Colonel Kurzen       (202)
 
-UPDATE `quest_template` SET `PrevQuestId` = 0   WHERE `entry` = 203;
-UPDATE `quest_template` SET `NextQuestId` = 204 WHERE `entry` = 203;
+UPDATE `quest_template` SET `PrevQuestId` = 0        WHERE `entry` = 203;
+UPDATE `quest_template` SET `NextQuestId` = 0        WHERE `entry` = 203;
+UPDATE `quest_template` SET `NextQuestInChain` = 204 WHERE `entry` = 203;
 
-UPDATE `quest_template` SET `PrevQuestId` = 203 WHERE `entry` = 204;
-UPDATE `quest_template` SET `NextQuestId` = 574 WHERE `entry` = 204;
+UPDATE `quest_template` SET `PrevQuestId` = 203      WHERE `entry` = 204;
+UPDATE `quest_template` SET `NextQuestId` = 0        WHERE `entry` = 204;
+UPDATE `quest_template` SET `NextQuestInChain` = 574 WHERE `entry` = 204;
 
-UPDATE `quest_template` SET `PrevQuestId` = 204 WHERE `entry` = 574;
-UPDATE `quest_template` SET `NextQuestId` = 202 WHERE `entry` = 574;
+UPDATE `quest_template` SET `PrevQuestId` = 204      WHERE `entry` = 574;
+UPDATE `quest_template` SET `NextQuestId` = 0        WHERE `entry` = 574;
+UPDATE `quest_template` SET `NextQuestInChain` = 202 WHERE `entry` = 574;
 
-UPDATE `quest_template` SET `PrevQuestId` = 574 WHERE `entry` = 202;
-UPDATE `quest_template` SET `NextQuestId` = 0   WHERE `entry` = 202;
+UPDATE `quest_template` SET `PrevQuestId` = 574      WHERE `entry` = 202;
+UPDATE `quest_template` SET `NextQuestId` = 0        WHERE `entry` = 202;
+UPDATE `quest_template` SET `NextQuestInChain` = 0   WHERE `entry` = 202;
