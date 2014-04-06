27bee42d0ba347d576da16c006a34e73ff226360
Sinoxan <sinoxan@googlemail.com>
2014-04-06 18:04:37 +0100

    Forgot Exclusive Group

--- updates/quest_template.sql
+++ updates/quest_template.sql
@@ -8,15 +8,19 @@
 UPDATE `quest_template` SET `PrevQuestId` = 0        WHERE `entry` = 203;
 UPDATE `quest_template` SET `NextQuestId` = 574      WHERE `entry` = 203;
 UPDATE `quest_template` SET `NextQuestInChain` = 574 WHERE `entry` = 203;
+UPDATE `quest_template` SET `ExclusiveGroup` = 203   WHERE `entry` = 203;
 
 UPDATE `quest_template` SET `PrevQuestId` = 0        WHERE `entry` = 204;
 UPDATE `quest_template` SET `NextQuestId` = 574      WHERE `entry` = 204;
 UPDATE `quest_template` SET `NextQuestInChain` = 574 WHERE `entry` = 204;
+UPDATE `quest_template` SET `ExclusiveGroup` = 203   WHERE `entry` = 204;
 
 UPDATE `quest_template` SET `PrevQuestId` = 204      WHERE `entry` = 574;
 UPDATE `quest_template` SET `NextQuestId` = 0        WHERE `entry` = 574;
 UPDATE `quest_template` SET `NextQuestInChain` = 202 WHERE `entry` = 574;
+UPDATE `quest_template` SET `ExclusiveGroup` = 0     WHERE `entry` = 574;
 
 UPDATE `quest_template` SET `PrevQuestId` = 574      WHERE `entry` = 202;
 UPDATE `quest_template` SET `NextQuestId` = 0        WHERE `entry` = 202;
 UPDATE `quest_template` SET `NextQuestInChain` = 0   WHERE `entry` = 202;
+UPDATE `quest_template` SET `ExclusiveGroup` = 0     WHERE `entry` = 202;
