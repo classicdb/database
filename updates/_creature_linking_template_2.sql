97ab1cdee34eb7903d665c5354e0f6fb0357b166
Tobschinski <tobschinski@gmail.com>
2014-04-10 11:51:36 +0200

    [] - Temple of Ahn'Qiraji - Changes to Obsidian Eradicator * new Linking

---
+++ updates/_creature_linking_template_2.sql
@@ -0,0 +1,5 @@
+-- Obsidian Eradicator
+
+DELETE FROM `creature_linking_template` WHERE `entry`='15262';
+
+INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range`) VALUES ('15262', '531', '15263', '1024', '0');
