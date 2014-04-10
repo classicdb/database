2a6f07a7cd53039d25d99b32939d2ee474eb18c6
Tobschinski <tobschinski@gmail.com>
2014-04-10 19:41:17 +0200

    [] - Temple of Ahn'Qiraji - Changes to Battleguard Sartura
    * Position
    * Movement
    * Stats, Speed & Respawn
    * Linking

---
+++ updates/_creature_template_2.sql
@@ -0,0 +1,4 @@
+-- Battleguard Sartura & Guards
+
+UPDATE `creature_template` SET `minhealth`='716400', `maxhealth`='716400', `speed_walk`='6' WHERE `entry`='15516';
+UPDATE `creature_template` SET `minhealth`='92880', `maxhealth`='92880', `speed_walk`='6' WHERE `entry`='15984';
