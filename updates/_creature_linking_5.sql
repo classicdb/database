98a41c9102c77ac0ebad79436ce9b68f0d57a170
Tobschinski <tobschinski@gmail.com>
2014-04-12 08:42:33 +0200

    [] - Temple of Ahn'Qiraji - Changes to Qiraji Slayer / Mindslayer
    * Position
    * Reputation
    * Stats, Respawn
    * Linking
    * Pooling

---
+++ updates/_creature_linking_5.sql
@@ -0,0 +1,77 @@
+-- Qiraji Mindslayer / Qiraji Slayer
+
+SET @guidstart := '590500';
+
+DELETE FROM `creature_linking` WHERE `master_guid` IN ('88035', '88057', '88039', '88058', '88036', '88037', '88038', '88030');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 0, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 1, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 2, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 3, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 4, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 5, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 6, '88035', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 7, '88035', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 8, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 9, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 10, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 11, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 12, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 13, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 14, '88057', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 15, '88057', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 16, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 17, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 18, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 19, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 20, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 21, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 22, '88039', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 23, '88039', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 24, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 25, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 26, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 27, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 28, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 29, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 30, '88058', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 31, '88058', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 32, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 33, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 34, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 35, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 36, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 37, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 38, '88036', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 39, '88036', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 40, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 41, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 42, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 43, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 44, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 45, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 46, '88037', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 47, '88037', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 48, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 49, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 50, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 51, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 52, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 53, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 54, '88038', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 55, '88038', '1679');
+
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 56, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 57, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 58, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 59, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 60, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 61, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 62, '88030', '1679');
+INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES (@guidstart + 63, '88030', '1679');
