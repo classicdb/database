fce2abbd39581c1972d7171e4f3a6d993063b34f
Tobschinski <tobschinski@gmail.com>
2014-04-29 17:27:24 +0200

    [] - Temple of Ahn'Qiraji - Changes to GOs
    * locked the large scarab coffers
    * idols in coffers are now lootable without being on a certain quest
    * corrected the rotation of doors and coffers

---
+++ updates/_gameobject_loot_template_2.sql
@@ -0,0 +1,8 @@
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='11' WHERE `entry`='17533' and`item`='20882';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='19' WHERE `entry`='17533' and`item`='20881';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='11' WHERE `entry`='17533' and`item`='20879';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='08' WHERE `entry`='17533' and`item`='20878';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='16' WHERE `entry`='17533' and`item`='20877';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='24' WHERE `entry`='17533' and`item`='20876';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='30' WHERE `entry`='17533' and`item`='20875';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='24' WHERE `entry`='17533' and`item`='20874';
---
+++ updates/_gameobject_template_1.sql
@@ -0,0 +1,1 @@
+UPDATE `gameobject_template` SET `flags`='2' WHERE `entry`='180690';
