da2b7dc7d921a18942b5954af8e66ab69314fdb2
Tobschinski <Tobschinski@users.noreply.github.com>
2014-05-12 22:10:31 +0200

    Update _creature.sql

--- updates/_creature.sql
+++ updates/_creature.sql
@@ -4,4 +4,3 @@ UPDATE `creature` SET `position_x`='-8586.890', `position_y`='1427.870', `positi
 UPDATE `creature` SET `position_x`='-8707.966', `position_y`='1495.304', `position_z`='32.0320',`MovementType`='2' WHERE `guid`='140283';
 UPDATE `creature` SET `position_x`='-8677.870', `position_y`='1540.740', `position_z`='31.9674',`MovementType`='2' WHERE `guid`='140284';
 UPDATE `creature` SET `position_x`='-8643.200', `position_y`='1418.830', `position_z`='32.9521',`MovementType`='2' WHERE `guid`='140285';
-
