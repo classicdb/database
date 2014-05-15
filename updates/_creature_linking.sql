49d9d8d9350d9876c47a8aa69c2298b5afa3d09b
Tobschinski <Tobschinski@users.noreply.github.com>
2014-05-15 13:50:28 +0200

    Update _creature_linking.sql

--- updates/_creature_linking.sql
+++ updates/_creature_linking.sql
@@ -3,8 +3,8 @@ SET @GUID := '590800';
 
 -- -------------------
 
-SET @MOAM := '90909';
-SET @BURU := '90873';
+SET @MOAM := (SELECT `guid` FROM `creature` WHERE `id`='15340');
+SET @BURU := (SELECT `guid` FROM `creature` WHERE `id`='15370');
 
 DELETE FROM `creature_linking` WHERE `guid` BETWEEN @GUID + 0 AND @GUID + 4;
 INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
