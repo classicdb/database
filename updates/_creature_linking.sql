8cb1bb20c1e2c05704db18aa56ea48595c1866a5
Tobschinski <tobschinski@gmail.com>
2014-05-23 03:42:18 +0200

    * fixed linked boss to General Rajaxx

--- updates/_creature_linking.sql
+++ updates/_creature_linking.sql
@@ -2,15 +2,15 @@
 -- ----------------------------------------
 
 SET @KURI := (SELECT `guid` FROM `creature` WHERE `id`='15348');
-SET @MOAM := (SELECT `guid` FROM `creature` WHERE `id`='15340');
-SET @BURU := (SELECT `guid` FROM `creature` WHERE `id`='15370');
+SET @GENR := (SELECT `guid` FROM `creature` WHERE `id`='15341');
+
 
 DELETE FROM `creature_linking` WHERE `guid` IN ('87524', '87521', '87519', '87518');
 INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
 ('87524', @KURI, '1024'),
 ('87521', @KURI, '1024'),
-('87519', @MOAM, '1024'),
-('87518', @BURU, '1024');
+('87519', @GENR, '1024'),
+('87518', @GENR, '1024');
 
 DELETE FROM `creature_linking` WHERE `guid` IN ('87523', '87522', '87520', '87517');
 INSERT INTO `creature_linking` (`guid`, `master_guid`, `flag`) VALUES
