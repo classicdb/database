3f8a951c18c1aaac4c4471804b6ef0e74ca04785
Tobschinski <tobschinski@gmail.com>
2014-06-30 14:00:25 +0200

    Stratholme - Rockwing Gargoyle / Rockwing Screecher
    * just one pool will spawn Stonespine
    * a few wp-fixes

--- updates/_pool_creature.sql
+++ updates/_pool_creature.sql
@@ -1,9 +1,10 @@
 
-SET @GUID := '700000'; -- 14 (21) creatures needed
-SET @POOL := '30000'; -- 1 needed
+SET @GUID := '700000'; -- 15
+SET @POOL := '30000'; -- 7 needed
 
 -- --------------------------------------------------
 
+DELETE FROM `pool_creature` WHERE `guid` BETWEEN @GUID + 0 AND @GUID + 14;
 INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES
 (@GUID + 00, @POOL + 0, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
 (@GUID + 07, @POOL + 0, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
@@ -11,8 +12,9 @@ INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALU
 (@GUID + 08, @POOL + 1, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
 (@GUID + 02, @POOL + 2, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
 (@GUID + 09, @POOL + 2, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
-(@GUID + 03, @POOL + 3, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
-(@GUID + 10, @POOL + 3, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
+(@GUID + 03, @POOL + 3, '40', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher / Stonespine'),
+(@GUID + 10, @POOL + 3, '40', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher / Stonespine'),
+(@GUID + 14, @POOL + 3, '20', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher / Stonespine'),
 (@GUID + 04, @POOL + 4, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
 (@GUID + 11, @POOL + 4, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
 (@GUID + 05, @POOL + 5, '50', 'Stratholme - Rockwing Gargoyle / Rockwing Screecher'),
