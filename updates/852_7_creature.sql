e4e1d720a5518f75473bcab6253b549c43cfb9e6
Zingzah <Octopussoir@bigpond.com>
2014-12-21 22:58:57 +1100

    [0852] - Gizelton Caravan
    Includes updates to
    *Northern and Southern stop points
    *Quest stop points
    *Super-seller 680 and Vendortron 1000 spawning and items
    *Bodyguards for Hire and Gizelton Caravan Quest availability

---
+++ updates/852_7_creature.sql
@@ -0,0 +1,6 @@
+/*Delete Super-seller and Vendortron guids from creature table*/
+DELETE FROM `mangos141`.`creature` WHERE `guid`=29117;
+DELETE FROM `mangos141`.`creature` WHERE `guid`=29119;
+
+
+
