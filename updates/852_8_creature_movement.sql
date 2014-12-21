b13f7012318337b866b5f6ede5c13467c0f4cb1d
Zingzah <Octopussoir@bigpond.com>
2014-12-21 22:44:16 +1100

    Gizelton Caravan
    Includes updates to
    *Northern and Southern stop points
    *Quest stop points
    *Superseller and Vendortron spawning and items
    *Quest availability

---
+++ updates/852_8_creature_movement.sql
@@ -0,0 +1,6 @@
+/*Removing Cork and Rigger Gizelton waypoints from creature movement table */
+DELETE FROM `mangos`.`creature_movement` WHERE  `id`=28714;
+DELETE FROM `mangos`.`creature_movement` WHERE  `id`=28728;
+
+
+
