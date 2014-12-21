f547ac5de05e3457baac5e8c4a671b93270835c4
Zingzah <Octopussoir@bigpond.com>
2014-12-21 22:45:15 +1100

    Revert "Gizelton Caravan"
    This reverts commit b13f7012318337b866b5f6ede5c13467c0f4cb1d.

--- updates/852_8_creature_movement.sql
+++
@@ -1,6 +0,0 @@
-/*Removing Cork and Rigger Gizelton waypoints from creature movement table */
-DELETE FROM `mangos`.`creature_movement` WHERE  `id`=28714;
-DELETE FROM `mangos`.`creature_movement` WHERE  `id`=28728;
-
-
-
--- updates/852_9_creature.sql
+++
@@ -1,6 +0,0 @@
-/*Delete Super-seller and Vendortron guids from creature table*/
-DELETE FROM `mangos141`.`creature` WHERE `guid`=29117;
-DELETE FROM `mangos141`.`creature` WHERE `guid`=29119;
-
-
-
