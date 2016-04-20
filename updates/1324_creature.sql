9279693a42dc1b1b1365ccd4e7df895a7e070d41
Phatcat <holsthein@live.dk>
2016-04-20 22:22:52 +0200

    creature: Kroshius (spawn location)
    Pull rrequest made on behalf of @killerwife correcting the spawn
    location of Kroshius.

---
+++ updates/1324_creature.sql
@@ -0,0 +1,2 @@
+-- This is a pr made on behalf of @killerwife, fixing the spawn location of Kroshius.
+UPDATE creature SET position_x = 5800.89, position_y = -985.82, position_z = 409.78 WHERE guid = 39715;
