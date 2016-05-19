6d6e38f7f56fb78b14f9865700685da320407f2f
Zooke <zookeori@hotmail.com>
2016-05-19 12:36:44 +0100

    Done.

--- updates/9999_zooke_protecting_the_shipment.sql
+++ updates/9999_zooke_protecting_the_shipment.sql
@@ -1,2 +1,2 @@
 -- Fix quest 309 Protecting the Shipment completion point with the patched SD2 script
-UPDATE `mangos`.`quest_template` SET `SpecialFlags`='2' WHERE `entry`='309';
+UPDATE `quest_template` SET `SpecialFlags`='2' WHERE `entry`='309';
